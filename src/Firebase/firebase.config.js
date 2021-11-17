1a1c6581c1e625b4ed501d2c18963c41c33ff588
unknown <hshanto707@gmail.com>
2021-11-17 14:55:31 +0600

    initial commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBbBW_lcmLEJLsfw2hPc0nfve0ai8-7iOM",
+  authDomain: "instagram-clone-hhs.firebaseapp.com",
+  projectId: "instagram-clone-hhs",
+  storageBucket: "instagram-clone-hhs.appspot.com",
+  messagingSenderId: "403697020354",
+  appId: "1:403697020354:web:cda2119720f1eccf943a5b"
+};
+
+export default firebaseConfig;
---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,7 @@
+import firebaseConfig from "./firebase.config";
+
+const firebaseInitialize = () => {
+  const app = initializeApp(firebaseConfig);
+}
+
+return default firebaseInitialize;
